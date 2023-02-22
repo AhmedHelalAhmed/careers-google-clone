e3d67bd84e8bb97a24d976bb39d52cbc610e70ee
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-22 23:30:15 +0200

    🧪 add test for navigation 🧪

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -2,9 +2,24 @@ import { render, screen } from "@testing-library/vue";
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
-  it("displays company name", function () {
+  it("displays company name", () => {
     render(MainNav);
     const companyName = screen.getByText("Bobo Careers");
     expect(companyName).toBeInTheDocument();
+  });
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuText = navigationMenuItems.map(
+      (navigationMenuItem) => navigationMenuItem.textContent
+    );
+    expect(navigationMenuText).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
