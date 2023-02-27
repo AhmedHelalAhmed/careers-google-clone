017f223086ef510b88f6eecc94b6a8fa42e6b57f
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-27 20:36:35 +0200

    🚀 add the hero and the headline componets 🚀

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <main-nav />
+  <the-hero></the-hero>
 </template>
 <script>
 import MainNav from "@/components/MainNav.vue";
+import TheHero from "@/components/TheHero.vue";
 
 export default {
   name: "App",
   components: {
+    TheHero,
     MainNav,
   },
 };
