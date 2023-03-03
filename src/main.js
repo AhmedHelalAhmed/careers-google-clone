2d8a61292a3180fbd38e6d0d04e1c75afecfa7fb
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-03 10:18:36 +0200

    📦 add pinia 📦

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "@/index.css";
 import router from "@/routes";
 import App from "@/App.vue";
+import { createPinia } from "pinia";
 
 library.add(faSearch);
+const pinia = createPinia();
 
 createApp(App)
+  .use(pinia)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
