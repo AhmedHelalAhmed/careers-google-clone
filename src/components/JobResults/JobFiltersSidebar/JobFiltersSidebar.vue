1e3949080bd721b008f1a8af73350e7265f157de
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-03 16:03:28 +0200

    ✨ add CollapsibleAccordion component ✨

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,13 +2,39 @@
   <div
     class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
   >
-    Job filters sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear filters" type="secondary"></action-button>
+        </div>
+      </div>
+      <collapsible-accordion header="Job types">
+        <p>placeholder</p>
+      </collapsible-accordion>
+      <collapsible-accordion header="Organizations">
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="h-8 w-1/2">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </collapsible-accordion>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
 export default {
   name: "JobFiltersSidebar",
+  components: { CollapsibleAccordion, ActionButton },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@/index.css";
 import router from "@/routes";
@@ -9,6 +13,8 @@ import App from "@/App.vue";
 import { createPinia } from "pinia";
 
 library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
 const pinia = createPinia();
 
 createApp(App)
