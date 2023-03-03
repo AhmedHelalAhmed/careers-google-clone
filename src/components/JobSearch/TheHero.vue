fefe19188659c89afa963467633411652310d1c0
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-03 20:58:46 +0200

    ♻️refactor ♻

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -18,20 +18,20 @@
       </div>
     </section>
     <spot-light class="flex flex-row justify-center pb-16">
-      <template #default="slotProps">
+      <template #default="{ img, title, description }">
         <!-- another design ...
         <router-link
           to="/jobs/results"
           class="mx-5 flex h-[268px] w-[530px] flex-col rounded-lg border bg-brand-gray-2"
         >
           <div
-            :style="{ backgroundImage: 'url(' + slotProps.spotlight.img + ')' }"
+            :style="{ backgroundImage: 'url(' + img + ')' }"
             class="flex h-full flex-col justify-end bg-cover"
           >
             <div class="text-white">
               <div class="px-6 py-4">
-                <h3 class="text-xl">{{ slotProps.spotlight.title }}</h3>
-                <p class="pt-4">{{ slotProps.spotlight.description }}</p>
+                <h3 class="text-xl">{{ title }}</h3>
+                <p class="pt-4">{{ description }}</p>
               </div>
             </div>
             <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-white"
@@ -44,10 +44,10 @@
           to="/jobs/results"
           class="mx-5 flex h-96 w-72 flex-col rounded-lg border bg-brand-gray-2"
         >
-          <img :src="slotProps.spotlight.img" alt="" class="object-contain" />
+          <img :src="img" alt="" class="object-contain" />
           <div class="mt-3 h-48 px-6 py-4">
-            <h3 class="text-xl font-medium">{{ slotProps.spotlight.title }}</h3>
-            <p class="mt-3 text-sm">{{ slotProps.spotlight.description }}</p>
+            <h3 class="text-xl font-medium">{{ title }}</h3>
+            <p class="mt-3 text-sm">{{ description }}</p>
           </div>
           <router-link
             to="/jobs/results"
