c14b0a1a76d2ff7b3e6a7a97643aa460390baa1a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-04 18:40:17 +0200

    🔥  remove unneeded code  🔥

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,7 +2,6 @@ import { defineStore } from "pinia";
 
 export const ADD_SELECTED_ORGANIZATIONS = "ADD_SELECTED_ORGANIZATIONS";
 export const ADD_SELECTED_JOB_TYPES = "ADD_SELECTED_JOB_TYPES";
-
 export const useUserStore = defineStore("user", {
   state: () => ({
     isLoggedIn: false,
