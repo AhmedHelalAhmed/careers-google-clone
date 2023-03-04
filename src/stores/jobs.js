8b85e185438b0a779924355603f916d58b90a55b
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-04 05:07:27 +0200

    ♻️refactor ♻

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -9,6 +9,8 @@ export const UNIQUE_JOB_TYPES = "UNIQUE_JOB_TYPES";
 export const FILTERED_JOBS_BY_ORGANIZATIONS = "FILTERED_JOBS_BY_ORGANIZATIONS";
 export const FILTERED_JOBS_BY_JOB_TYPES = "FILTERED_JOBS_BY_JOB_TYPES";
 export const FILTERED_JOBS = "FILTERED_JOBS";
+export const INCLUDE_JOB_BY_JOB_TYPE = "INCLUDE_JOB_BY_JOB_TYPE";
+export const INCLUDE_JOB_BY_BY_ORGANIZATION = "INCLUDE_JOB_BY_BY_ORGANIZATION";
 
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
@@ -49,23 +51,24 @@ export const useJobsStore = defineStore("jobs", {
         userStore.selectedJobTypes.includes(job.jobType)
       );
     },
-    [FILTERED_JOBS](state) {
+    [INCLUDE_JOB_BY_BY_ORGANIZATION]: () => (job) => {
       const userStore = useUserStore();
-      const noSelectedOrganizations =
-        userStore.selectedOrganizations.length === 0;
-      const noSelectedJobTypes = userStore.selectedJobTypes.length === 0;
-
+      if (userStore.selectedOrganizations.length === 0) {
+        return true;
+      }
+      return userStore.selectedOrganizations.includes(job.organization);
+    },
+    [INCLUDE_JOB_BY_JOB_TYPE]: () => (job) => {
+      const userStore = useUserStore();
+      if (userStore.selectedJobTypes.length === 0) {
+        return true;
+      }
+      return userStore.selectedJobTypes.includes(job.jobType);
+    },
+    [FILTERED_JOBS](state) {
       return state.jobs
-        .filter((job) =>
-          noSelectedOrganizations
-            ? true
-            : userStore.selectedOrganizations.includes(job.organization)
-        )
-        .filter((job) =>
-          noSelectedJobTypes
-            ? true
-            : userStore.selectedJobTypes.includes(job.jobType)
-        );
+        .filter((job) => this.INCLUDE_JOB_BY_BY_ORGANIZATION(job))
+        .filter((job) => this.INCLUDE_JOB_BY_JOB_TYPE(job));
     },
   },
 });
