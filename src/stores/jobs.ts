450490ab2181555f3973f1af8d23f55fb44ab630
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-07 00:17:06 +0200

    ♻️   refactor JobsStore  ♻️

--- src/stores/jobs.ts
+++ src/stores/jobs.ts
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import getJobs from "@/api/getJobs";
 import { useUserStore } from "@/stores/user";
 import type { Job } from "@/api/types";
+import { computed, ref } from "vue";
 
 export const FETCH_JOBS = "FETCH_JOBS";
 export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
@@ -12,64 +13,64 @@ export const INCLUDE_JOB_BY_JOB_TYPE = "INCLUDE_JOB_BY_JOB_TYPE";
 export const INCLUDE_JOB_BY_ORGANIZATION = "INCLUDE_JOB_BY_ORGANIZATION";
 export const INCLUDE_JOB_BY_DEGREE = "INCLUDE_JOB_BY_DEGREE";
 export const INCLUDE_JOB_BY_SKILL = "INCLUDE_JOB_BY_SKILL";
-
-export interface JobState {
-  jobs: Job[];
-}
-
-export const useJobsStore = defineStore("jobs", {
-  state: (): JobState => ({
-    jobs: [],
-  }),
-  actions: {
-    async [FETCH_JOBS]() {
-      this.jobs = await getJobs();
-    },
-  },
-  getters: {
-    [UNIQUE_ORGANIZATIONS](state) {
-      const uniqueOrganizations = new Set<string>();
-      state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
-      return uniqueOrganizations;
-    },
-    [UNIQUE_JOB_TYPES](state) {
-      const uniqueJobTypes = new Set<string>();
-      state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType));
-      return uniqueJobTypes;
-    },
-    [INCLUDE_JOB_BY_ORGANIZATION]: () => (job: Job) => {
-      const userStore = useUserStore();
-      if (userStore.selectedOrganizations.length === 0) {
-        return true;
-      }
-      return userStore.selectedOrganizations.includes(job.organization);
-    },
-    [INCLUDE_JOB_BY_JOB_TYPE]: () => (job: Job) => {
-      const userStore = useUserStore();
-      if (userStore.selectedJobTypes.length === 0) {
-        return true;
-      }
-      return userStore.selectedJobTypes.includes(job.jobType);
-    },
-    [INCLUDE_JOB_BY_DEGREE]: () => (job: Job) => {
-      const userStore = useUserStore();
-      if (userStore.selectedDegrees.length === 0) {
-        return true;
-      }
-      return userStore.selectedDegrees.includes(job.degree);
-    },
-    [INCLUDE_JOB_BY_SKILL]: () => (job: Job) => {
-      const userStore = useUserStore();
-      return job.title
-        .toLowerCase()
-        .includes(userStore.skillsSearchTerm.toLowerCase());
-    },
-    [FILTERED_JOBS](state): Job[] {
-      return state.jobs
-        .filter((job) => this.INCLUDE_JOB_BY_ORGANIZATION(job))
-        .filter((job) => this.INCLUDE_JOB_BY_JOB_TYPE(job))
-        .filter((job) => this.INCLUDE_JOB_BY_DEGREE(job))
-        .filter((job) => this.INCLUDE_JOB_BY_SKILL(job));
-    },
-  },
+export const useJobsStore = defineStore("jobs", () => {
+  const jobs = ref<Job[]>([]);
+  const FETCH_JOBS = async () => {
+    jobs.value = await getJobs();
+  };
+  const UNIQUE_ORGANIZATIONS = computed(() => {
+    const uniqueOrganizations = new Set<string>();
+    jobs.value.forEach((job) => uniqueOrganizations.add(job.organization));
+    return uniqueOrganizations;
+  });
+  const UNIQUE_JOB_TYPES = computed(() => {
+    const uniqueJobTypes = new Set<string>();
+    jobs.value.forEach((job) => uniqueJobTypes.add(job.jobType));
+    return uniqueJobTypes;
+  });
+  const INCLUDE_JOB_BY_ORGANIZATION = (job: Job) => {
+    const userStore = useUserStore();
+    if (userStore.selectedOrganizations.length === 0) {
+      return true;
+    }
+    return userStore.selectedOrganizations.includes(job.organization);
+  };
+  const INCLUDE_JOB_BY_JOB_TYPE = (job: Job) => {
+    const userStore = useUserStore();
+    if (userStore.selectedJobTypes.length === 0) {
+      return true;
+    }
+    return userStore.selectedJobTypes.includes(job.jobType);
+  };
+  const INCLUDE_JOB_BY_DEGREE = (job: Job) => {
+    const userStore = useUserStore();
+    if (userStore.selectedDegrees.length === 0) {
+      return true;
+    }
+    return userStore.selectedDegrees.includes(job.degree);
+  };
+  const INCLUDE_JOB_BY_SKILL = (job: Job) => {
+    const userStore = useUserStore();
+    return job.title
+      .toLowerCase()
+      .includes(userStore.skillsSearchTerm.toLowerCase());
+  };
+  const FILTERED_JOBS = computed(() => {
+    return jobs.value
+      .filter((job: Job) => INCLUDE_JOB_BY_ORGANIZATION(job))
+      .filter((job: Job) => INCLUDE_JOB_BY_JOB_TYPE(job))
+      .filter((job: Job) => INCLUDE_JOB_BY_DEGREE(job))
+      .filter((job: Job) => INCLUDE_JOB_BY_SKILL(job));
+  });
+  return {
+    jobs,
+    FETCH_JOBS,
+    UNIQUE_ORGANIZATIONS,
+    UNIQUE_JOB_TYPES,
+    INCLUDE_JOB_BY_ORGANIZATION,
+    INCLUDE_JOB_BY_JOB_TYPE,
+    INCLUDE_JOB_BY_DEGREE,
+    INCLUDE_JOB_BY_SKILL,
+    FILTERED_JOBS,
+  };
 });
